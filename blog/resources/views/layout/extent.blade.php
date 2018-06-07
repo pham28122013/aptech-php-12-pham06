5dd64896fcfbdd726361ebf450c6c69ace7d481b
pham28122013 <pham28122013>
2018-06-07 16:18:43 +0700

    bai tap

---
+++ blog/resources/views/layout/extent.blade.php
@@ -0,0 +1,83 @@
+
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="container">
+    <div class="d-flex flex-row">
+
+          
+                <div class="col-sm-4 d-flex align-items-center ">
+                    <h3>Subscribe</h3>
+                </div>
+                <div class="col-sm-4 d-flex justify-content-center">
+                    <h1><b>Large</b></h1>
+                </div>
+                <div class="col-sm-4 d-flex  justify-content-end align-items-center">
+                    <i class="fas fa-search"></i>
+                    <a href="#">Singn up</a>
+                </div>
+                  
+      
+    </div>
+<div class="content">
+    <hr>
+    <ul class="nav d-flex justify-content-between">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">word</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">u.s</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">technogo</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Disign</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="#">culture</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Disign</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">culture</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">culture</a>
+            </li>
+    </ul>
+    <div class="images">
+            <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+                    <div class="col-md-6 px-0">
+                      <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
+                      <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p>
+                      <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
+                    </div>
+                  </div>
+    </div>
+</div>
+<div class="content">
+    @yield('content')
+</div>
+</div>
+<div class="footer">
+    <hr>
+    <div class="d-flex justify-content-center">
+         Blog template built for  <a href="#"> Bootstrap </a> by <a href="#"> @mdo. </a></br>
+    </div>
+    <div class="d-flex justify-content-center">
+    <a href="#">Back to top</a>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ blog/resources/views/parent.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.extent')
+@section('content')
+<a>Cum sociis natoque penatibus et magnis dis parturient montes,
+         nascetur ridiculus mus. Aenean lacinia bibendum nulla sed 
+         consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus 
+        commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</a>
+        @endsection
